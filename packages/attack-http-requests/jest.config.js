f884eafaff415c54433b7825b980b0f2feb2aeda
codefactor-io <support@codefactor.io>
2024-07-18 11:32:15 +0000

    [CodeFactor] Apply fixes

--- packages/attack-http-requests/jest.config.js
+++ packages/attack-http-requests/jest.config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    testEnvironment: 'node',
-    clearMocks: true,
-    collectCoverage: true,
-    coverageReporters: ['text', 'html', 'lcov'],
-    transform: {
-      '^.+\\.ts$': 'ts-jest',
-    },
-    testRegex: '.test.[tj]s$',
-    moduleFileExtensions: ['js', 'ts'],
-  };
+  testEnvironment: 'node',
+  clearMocks: true,
+  collectCoverage: true,
+  coverageReporters: ['text', 'html', 'lcov'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  testRegex: '.test.[tj]s$',
+  moduleFileExtensions: ['js', 'ts'],
+};
